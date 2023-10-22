bc2660ab07b41c7c045e11e4cdc9bdadb50f6065
MonaemKhan <khanmonaem07@gmail.com>
2023-10-22 22:09:09 +0600

    riyaz vai project

---
+++ Next JS/Riyaz Vai Project/tactsoft-hrm/src/pages/admin/monaem/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Monaem = () => {
+    return (
+        <div>
+            <h1>Big Congratulation From M.A. Monaem Khan</h1>
+        </div>
+    );
+};
+
+export default Monaem;
